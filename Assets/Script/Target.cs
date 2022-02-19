7c58a83082ab96620114e53103c95ad849a1a497
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2022-02-19 22:41:22 +0530

    Initial

---
+++ Assets/Script/Target.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float health =100f;
+    public void TakeDamage(float amount){
+        health -= amount;
+        if(health <= 0f){
+            Die();
+        }
+    }
+    void Die(){
+        Destroy(gameObject);
+    }
+}
